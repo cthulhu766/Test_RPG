11260674f98b1445858f8bfb75eefc7d68c40002
Dmytro Volynets <tortikzz93@gmail.com>
2023-01-22 20:28:32 +0200

    Додано босса

--- Test RPG.py
+++ Test RPG.py
@@ -5,6 +5,7 @@ import random
 class Creator():  # Створюємо класс істот
     def __init__(self, life, defence, damage):
         self.level = 1
+        self.boss_level = 1
         self.experience = 0
         self.life = life
         self.defence = defence
@@ -28,6 +29,55 @@ class Creator():  # Створюємо класс істот
 hero = Creator(100, 10, 40)  # параметри героя
 creature = Creator(40, 10, 20)  # параметри істоти
 
+def show(name):
+    print(f'''
+    life: {name.life}
+    defence: {name.damage}
+    damage: {name.defence}
+    critical hit: {name.critical}
+    penetrating: {name.penetrating}
+    ''')
+
+def attacking_boss():
+    boss = Creator(100 + hero.boss_level * 10, 20 + hero.boss_level * 2, 35 + hero.boss_level * 3)
+    print('Hero parameters:')
+    show(hero)
+    print('Boss parameters:')
+    show(boss)
+    hero_life = hero.life
+    version = int(input('''
+        1. Show the details
+        2. Don`t show the details
+        '''))
+    # детальний бій (кто кого б'є і т.д.)
+    if version == 1:
+        while True:
+            input('Press Enter to attack')
+            time.sleep(2)
+            hero.attack(boss)
+            if boss.life <= 0:
+                print('The boss is dead')
+                hero.life = hero_life
+                hero.boss_level +=1
+                break
+
+            print(f'Boss has {boss.life} life')
+            time.sleep(2)
+            print('Now the boss is attacking you.')
+            time.sleep(2)
+            boss.attack(hero)
+
+            if hero.life <= 0:
+                print('Hero is dead')
+                hero.life = hero_life
+                break
+            print(f'Hero has {hero.life} life')
+
+    # прискорена версія бою (треба пофіксити)
+    elif version == 2:
+        pass
+    if hero.boss_level > 99:
+        print('It is the last boss')
 
 def increase_level():
     hero.level += 1
@@ -74,10 +124,11 @@ def attack_creature():
             if creature.life <= 0 and creature_number == 1:
 
                 hero.experience += number_expirience*50
-
+                hero.life = hero_life
                 if hero.experience >= hero.level*100:
-                    hero.life = hero_life
+
                     increase_level()
+                    hero.experience = 0
 
                 print('Creature is dead, You Won!')
 
@@ -96,6 +147,7 @@ def attack_creature():
 
             if hero.life <= 0:
                 print('Hero is dead')
+                hero.life = hero_life
                 break
             print(f'Hero has {hero.life} life')
 
@@ -105,14 +157,21 @@ def attack_creature():
 
 
 def main():
-    print('''
-    1 Attack Creatures
-    2 Attack Boss
-    ''')
-    choice = int(input('What do you want to do?'))
-    if choice == 1:
-        attack_creature()
+    while True:
+
+        print('''
+        1 Attack Creatures
+        2 Attack Boss
+        3 Exit
+        ''')
+        choice = int(input('What do you want to do?'))
+        if choice == 1:
+            attack_creature()
+        elif choice == 2:
+            attacking_boss()
+        elif choice == 3:
+            break
 
 
-while True:
-    main()
+
+main()
